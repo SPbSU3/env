940a076225bfd872312636f43ee88c561d4798ca
PavelSavchenkov <pavelsavchenkow@gmail.com>
2018-03-08 21:53:22 +0300

    add c++ code template

---
+++ code_templates/template.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define ford(i, n) for (int i = int(n) - 1; i >= 0; --i)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) int((c).size())
+#define pb push_back
+#define mp make_pair
+
+#define FILE "a"
+
+#ifdef LOCAL
+#define eprintf(args...) fprintf(stderr, args), fflush(stdout)
+#else
+#define eprintf(args...) ;
+#endif
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+
+int n;
+
+bool read() {
+    if (!(cin >> n)) {
+        return 0;
+    }
+    return 1;
+}
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen(FILE ".in", "r", stdin);
+    freopen(FILE ".out", "w", stdout);
+#endif
+
+    while (read()) {
+        solve();
+    }
+
+#ifdef LOCAL
+    eprintf("Time %.5f\n", clock() * 1.0 / CLOCKS_PER_SEC);
+#endif
+}
+
